4fac99961ad35a1017eb2e7f9614c1683e20785b
Ersultan-collab <149644341+Ersultan-collab@users.noreply.github.com>
2023-11-19 14:27:23 +0600

    hw

---
+++ python/dictionaries.py
@@ -0,0 +1,22 @@
+dog = {'name':'Aktaban','color':'white','breed':'German Shepherd','age':'10',}
+
+
+student_dictionary = {'first_name':'Ersultan','last_name':'Marat','gender':'man','skills':['Programming', 'Mathematics', 'Communication'],'age':'18','marital status':'not married','country':'Kazakhstan','city':'Almaty',}
+# print(len(student_dictionary))
+
+# print(student_dictionary['skills'])
+
+# student_dictionary['skills'].append("program")
+# print(student_dictionary)
+# print(student_dictionary.keys())
+
+# print(student_dictionary.values())
+
+# print(student_dictionary.items())
+
+# student_dictionary.pop('first_name')
+# print(student_dictionary)
+
+del dog
+print(dog)
+print(student_dictionary)
