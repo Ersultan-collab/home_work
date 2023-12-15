eccb2a20962f28d96a1ed7cf97403f2dda10f304
Ersultan-collab <149644341+Ersultan-collab@users.noreply.github.com>
2023-12-15 21:18:21 +0600

    hw_14.12.23

---
+++ js/hw_14.12.23/index.js
@@ -0,0 +1,77 @@
+// let challenge = '30 Days Of JavaScript'
+// let it = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+// console.log(challenge.length)
+// console.log(challenge.toLocaleUpperCase())
+// console.log(challenge.toLocaleLowerCase())
+
+// console.log(challenge.substr(0,5))
+// console.log(challenge.substring(0,5))
+// console.log(challenge.substring(0,2))
+// console.log(challenge.includes('Script'))
+// let text = '30, Days, Of, JavaScript'
+// console.log(text.split(','))
+// console.log(it.split(','))
+// console.log(challenge.replace('JavaScript', 'Python'))
+// console.log(challenge.charAt(15))
+// console.log(challenge.charCodeAt('J'))
+// console.log(challenge.indexOf('a'))
+// console.log(challenge.lastIndexOf('a'))
+
+// let because = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(because.indexOf('because'))
+// console.log(because.lastIndexOf('because'))
+// console.log(because.search('because'))
+
+// let tr = ' 30 Days Of JavaScript '
+// console.log(tr.trim())
+
+// console.log(challenge.startsWith(30))
+// console.log(challenge.endsWith('pt'))
+// console.log(challenge.match('a'))
+
+// let days = '30 Days Of'
+// let java = ' JavaScript'
+// console.log(days.concat(java))
+// console.log(challenge.repeat(2))
+
+
+// console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+// console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
+
+// let floatValue = parseFloat('1,11')
+
+// if (floatValue != 10) {
+//     floatValue = 10
+//     console.log(floatValue)
+// }
+// else {
+//     console.log('true')
+// }
+
+// let string1 = 'python'
+// let string2 = 'jargon'
+
+// console.log(string1.includes('on'),string2.includes('on'))
+
+// let str = 'I hope this course is not full of jargon.'
+// console.log(str.includes('jargon'))
+
+// let str = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(str.substring(31,54))
+
+// let str = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+// console.log(str.match(/love/gi).length)
+
+// let str = 'You cannot end a sentence with because because because is a conjunction'
+// console.log(str.match(/because/gi).length)
+
+
+
+
+// console.log(
+//    ` ${1 * 1} ${1} ${1 * 1} ${1**3} 
+//  ${2 * 1} ${1} ${2**2} ${2**3}
+//  ${3 * 1} ${1} ${3**2} ${3**3}
+//  ${4 * 1} ${1} ${4**2} ${4**3}
+//  ${5 * 1} ${1} ${5**2} ${5**3} `
+// )
