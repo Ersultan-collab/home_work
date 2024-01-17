019ed71398560fc697dfd7e2b554d395ca79b74b
Ersultan-collab <149644341+Ersultan-collab@users.noreply.github.com>
2024-01-17 10:10:32 +0600

    hw

--- js/hw_16.01.24/index.js
+++ js/hw_16.01.24/index.js
@@ -4,13 +4,13 @@
 // }
 // console.log(findMax(40, 10, 30))
 
-// function printArray(arr){
-//     for(let i = 0; i < arr.length; i++){
-//         console.log(arr[i])
-//     }
-// }
-// let array = [12, 'dihehf', 20]
-// console.log(printArray(array))
+function printArray(arr){
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+}
+let array = [12, 'dihehf', 20]
+console.log(printArray(array))
 
 // function showDateTime(){
 //     let data = new Date()
@@ -38,3 +38,16 @@
 // console.log(solveLinEquation(2,3,6,5,8))
 
 
+// function num(num1){
+//     if(num1 % 2 == 1){
+//         return num1 * 0
+//     }else{
+//         return num1 * 2
+//     }
+// }
+// console.log(num(6))
+
+// function perimetr(a){
+//     return a + a + a + a
+// }
+// console.log(perimetr(5))
