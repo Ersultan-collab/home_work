8583979ea4f5d906edb566b0a84c708899dac754
Ersultan-collab <149644341+Ersultan-collab@users.noreply.github.com>
2024-01-17 08:46:10 +0600

    hw

---
+++ js/hw_16.01.24/index.js
@@ -0,0 +1,40 @@
+// function findMax(num1, num2, num3){
+//     let max = Math.max(num1, num2, num3)
+//     return max
+// }
+// console.log(findMax(40, 10, 30))
+
+// function printArray(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         console.log(arr[i])
+//     }
+// }
+// let array = [12, 'dihehf', 20]
+// console.log(printArray(array))
+
+// function showDateTime(){
+//     let data = new Date()
+//     let day = data.getDate()
+//     let month = data.getMonth()
+//     let year = data.getFullYear()
+//     let hour = data.getHours()
+//     let minute = data.getMinutes()
+//     let result = `${day}/${month}/${year} ${hour}:${minute}`
+//     return result
+// }
+// console.log(showDateTime())
+
+// function reverseArray(arr){
+//     for(let i = arr.length - 1; i >= 0; i--){
+//         console.log(arr[i])
+//     }
+//     return arr
+// }
+// console.log(reverseArray([1,2,3,4,5,6,7]))
+
+// function solveLinEquation(a, x, b, y, c){
+//     return a*x + b*y + c
+// }
+// console.log(solveLinEquation(2,3,6,5,8))
+
+
